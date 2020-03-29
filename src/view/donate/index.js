dbd50bf0552cf085ae9cf6cd0aff7f0e00846869
Raian de Andrades <raian.andrades@gmail.com>
2020-03-29 00:27:26 -0300

    Donate - progress

--- src/view/donate/index.js
+++ src/view/donate/index.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import OngService from "../../services/OngService";
 
 export default function Donate(props) {
+  const [ongs, setOngs] = useState([]);
+  const [error, setError] = useState(false);
+
+  const getOngs = () => {
+    OngService.getOngs()
+      .then(response => {
+        setOngs([]);
+      })
+      .catch(error => {
+        setError(true);
+      });
+  };
+
+  useEffect(() => {
+    getOngs();
+  }, []);
+
   return (
     <section className="donate">
       <h1>Lista de entidades para ajudar</h1>
+      <table>
+        <tbody>
+          {ongs.map((ong, index) => {
+            return (
+              <tr key={index}>
+                <td>{ong.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 }
