8237e181df5a906b71262bcee4f3fff55b16fc32
Lucas Rafael Gonçalves <lrafaelgoncalves@gmail.com>
2020-03-29 03:08:05 -0300

    Layout de login e componentes de botao e input

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,17 +1,50 @@
 import React from "react"
-import API from '../../api'
+import './style.scss'
+
+import { Button, Input } from '../../components'
 
 export default function Login(props) {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
 
-    const getLogin = async () => {
+  const handleLoginSubmit = async event => {
+    event.preventDefault()
 
-        const res = await API.post('/test')
-        console.log(res)
+    const data = {
+      name,
+      email
     }
 
+    try {
+      console.log(data)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
-    return (
-
-        <div>Login</div>
-    )
+  return (
+    <div className="Login">
+      <form onSubmit={handleLoginSubmit}>
+        <section>
+          <h1>Login</h1>
+          <div className="input-group">
+            <Input
+              label="Nome"
+              value={name}
+              onChange={({ target }) => setName(target.value)}
+              required
+            />
+            <Input
+              label="Email"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              type="email"
+              required
+            />
+          </div>
+          <Button type="submit">Entrar</Button>
+        </section>
+      </form>
+    </div>
+  )
 }
