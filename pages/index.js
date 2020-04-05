943ba88b0a1945aba1699cec362a90738f37e257
Matheus Ligabue da Silva <hi@matheusligabue.me>
2020-04-04 22:22:35 -0300

    Add footer, new home text

--- pages/index.js
+++ pages/index.js
@@ -30,32 +30,28 @@ const Home = ({ campaigns }) => (
       }
     >
       <Section id="apresentação" fontFamily="body" textAlign="left">
-        <Stack>
-          <Heading as="h3" fontSize={3}>
+        <Stack fontSize={3}>
+          <Heading as="h3" fontSize={4}>
             Olá, que bom que você está aqui!
           </Heading>
+
           <Text as="p">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
-            augue congue, efficitur massa a, consectetur urna. Morbi vehicula
-            sit amet est vehicula vulputate. Vivamus quis lacus fringilla,
-            pharetra leo sit amet, tincidunt ligula. Aliquam venenatis turpis
-            est, ut elementum orci fermentum eu. Pellentesque quis lacus nulla.
-            Sed faucibus mollis molestie. Duis in iaculis ex. Phasellus ut velit
-            semper, pellentesque nibh non, dapibus erat. Donec bibendum, velit
-            ac venenatis condimentum, metus magna elementum justo, eu convallis
-            nisl eros at lacus. Orci varius natoque penatibus et magnis dis
-            parturient montes, nascetur ridiculus mus.
+            Muitas pessoas e famílias inteiras estão passando por dificuldades
+            com as restrições impostas pela pandemia do COVID-19. O isolamento
+            social é necessário, mas com isso os empregos, atividades autônomas
+            e informais são afetados, impossibilitando muitos de ganhar o
+            próprio sustento. Isso restringe seus acessos ao que há de mais
+            básico: alimentação e itens higiene.
           </Text>
+
           <Text as="p">
-            Nam venenatis nec justo eu finibus. Nullam venenatis, justo id
-            mattis egestas, nisl felis dictum quam, ac posuere risus est ac
-            erat. Proin condimentum tristique velit, a gravida orci euismod et.
-            Integer quis magna nisl. Suspendisse ligula nibh, ultrices et ex
-            vitae, aliquet consequat quam. Sed vestibulum at dolor ac euismod.
+            Nesta página, reunimos algumas das iniciativas que estão ocorrendo
+            aqui em Porto Alegre para ajudá-lo a doar para quem mais necessita.
           </Text>
+
           <Text as="p">
-            Mauris sit amet commodo arcu. In tellus arcu, viverra eu aliquet
-            nec, malesuada eget urna. Aliquam facilisis condimentum luctus!
+            Este é um movimento de pessoas para pessoas! <br />
+            Colabore e nos ajude a potencializar essa causa do bem {"<3"}
           </Text>
         </Stack>
       </Section>
@@ -64,24 +60,14 @@ const Home = ({ campaigns }) => (
           <Heading as="h2" variant="headingHero" textAlign="center">
             Campanhas e ações de arrecadação abertas neste momento
           </Heading>
-          <Box variant="full-bleed" px={4}>
-            <Box variant="container-xl">
-              <Flex flexDirection="row" flexWrap="wrap" m={-2}>
-                {campaigns.map((campaign) => {
-                  console.log(campaign);
-                  return (
-                    <Box width={[1, 1, 1 / 2]} p={2} key={campaign._id}>
-                      <CampaignCard {...campaign} />
-                    </Box>
-                  );
-                })}
-              </Flex>
-            </Box>
-          </Box>
+          <Flex flexDirection="row" flexWrap="wrap" mx={-4}>
+            {campaigns.map((campaign) => (
+              <Box width={[1, 1, 1 / 2]} p={2} key={campaign._id}>
+                <CampaignCard {...campaign} />
+              </Box>
+            ))}
+          </Flex>
         </Stack>
-      </Section>
-      <Section as="footer" id="rodape" bg="text" color="accent">
-        <Heading>[Aqui vai o rodapé]</Heading>
       </Section>
     </NavBarLayout>
   </>
